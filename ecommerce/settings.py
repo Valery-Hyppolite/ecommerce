2870c32a543337399886a89d602314b6195afd17
Valery <valeryhypp@gmail.com>
2023-01-11 11:47:45 -0500

    add allow host to env file

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -13,7 +13,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = True
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',')
+#ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'e-furnish.herokuapp.com']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
